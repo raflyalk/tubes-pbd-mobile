5c53704d5cf34c48fc0ecb26a9f0d6eaf1081504
Aufa45P <13516008@std.stei.itb.ac.id>
2019-02-24 16:22:43 +0700

    Fix Redirect Problem

---
+++ app/src/main/java/com/example/freya/StartPageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.freya
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class StartPageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_page)
+    }
+}
