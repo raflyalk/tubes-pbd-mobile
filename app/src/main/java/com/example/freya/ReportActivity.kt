b769e4b4df10b0d53610fc91d217e0a65c5216b3
Aufa45P <13516008@std.stei.itb.ac.id>
2019-02-25 16:38:17 +0700

    Gradle Update location

--- app/src/main/java/com/example/freya/ReportActivity.kt
+++ app/src/main/java/com/example/freya/ReportActivity.kt
@@ -2,11 +2,16 @@ package com.example.freya
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class ReportActivity : AppCompatActivity() {
+
+    private lateinit var locationClient : FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report)
+        locationClient = LocationServices.getFusedLocationProviderClient(this)
     }
 }
